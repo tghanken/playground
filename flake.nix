b7b2994f5bdf9c8139fe26ca3f3864d2252450d5
Taylor Hanken <tghanken@gmail.com>
2024-08-11 21:03:10 -0700

    Initial flake setup

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      defaultPackage = pkgs.hello;
+      pkgs.devShell = pkgs.mkShell {
+        buildInputs = with pkgs;
+          [
+          ];
+      };
+    }
+  );
+}
