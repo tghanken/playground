dc88eb50745fc3dfddb6f11f39bbcde0820a2417
Taylor Hanken <tghanken@gmail.com>
2024-10-08 13:57:17 -0700

    feat: start scaffolding monorepo

---
+++ machines/machines.nix
@@ -0,0 +1,21 @@
+# The importApply argument. Use this to reference things defined locally,
+# as opposed to the flake where this is imported.
+localFlake:
+# Regular module arguments; self, inputs, etc all reference the final user flake,
+# where this module was imported.
+{
+  lib,
+  config,
+  self,
+  inputs,
+  ...
+}: {
+  flake = {
+    nixosConfigurations = {
+      nixos-usb = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [./hosts/nixos-usb/configuration.nix];
+      };
+    };
+  };
+}
