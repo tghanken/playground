6910be8d2c6547b5d5c71edf2de8de0ca9c8bed6
Taylor Hanken <tghanken@gmail.com>
2024-10-08 15:58:42 -0700

    add nixos-thinkpad config

--- machines/machines.nix
+++ machines/machines.nix
@@ -31,6 +31,31 @@ localFlake:
           }
         ];
       };
+      nixos-thinkpad = inputs.nixpkgs.lib.nixosSystem {
+        modules = with inputs; [
+          ./hosts/nixos-thinkpad/configuration.nix
+          ./common/common.nix
+          ../secrets/mod.nix
+
+          nix-serve-ng.nixosModules.default
+          agenix.nixosModules.default
+          {
+            # TODO: Split this into a flake-part module
+            environment.systemPackages = [agenix.packages."x86_64-linux".default];
+          }
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.tghanken = import ./home-manager/home.nix;
+          }
+          disko.nixosModules.disko
+          {
+            disko.devices = import ./hosts/nixos-thinkpad/devices.nix;
+          }
+        ];
+      };
       nixos-usb = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
