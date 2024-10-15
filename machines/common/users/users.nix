c55c67fb4915bfbd8b075a506b09272369b18144
Taylor Hanken <tghanken@gmail.com>
2024-10-15 11:27:06 -0700

    chore: add root password (#6)

--- machines/common/users/users.nix
+++ machines/common/users/users.nix
@@ -11,4 +11,5 @@
     # Create with mkpasswd -m sha-512
     hashedPassword = "$6$aVX13r8lw5yvxWJZ$TrXrqKub2dJArKGyZ75l5AQC.yIh8ysgigZniYT.ZkvQRvjgb45oFNUnFIUd5xTfE0JXzFzPHwMWdJcdth9Tj1";
   };
+  users.users.root.hashedPassword = "$6$aVX13r8lw5yvxWJZ$TrXrqKub2dJArKGyZ75l5AQC.yIh8ysgigZniYT.ZkvQRvjgb45oFNUnFIUd5xTfE0JXzFzPHwMWdJcdth9Tj1";
 }
