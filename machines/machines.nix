f05b7d8c197014ebb31fd8838adf1e1119c8420f
Taylor Hanken <tghanken@gmail.com>
2024-10-25 12:09:21 -0700

    fix missing =

--- machines/machines.nix
+++ machines/machines.nix
@@ -43,7 +43,7 @@ in {
       nixos-rpi3 = inputs.nixpkgs.lib.nixosSystem {
         modules = [./hosts/servers/nixos-rpi3/configuration.nix] ++ server_mods;
       };
-      nixos-rpi4-3 inputs.nixpkgs.lib.nixosSystem {
+      nixos-rpi4-3 = inputs.nixpkgs.lib.nixosSystem {
         modules = [./hosts/servers/nixos-rpi3/configuration.nix] ++ server_mods;
       };
       syno-vm = inputs.nixpkgs.lib.nixosSystem {
