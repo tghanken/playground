e4da3d979cbacaa874fc54dfdbde0a3f173af2ac
Taylor Hanken <tghanken@gmail.com>
2024-10-26 11:56:58 -0700

    simplify pi4 config

--- machines/modules/hardware/raspberry-pi/pi4.nix
+++ machines/modules/hardware/raspberry-pi/pi4.nix
@@ -1,12 +1,5 @@
 {
   imports = [./pi_common.nix];
 
-  hardware = {
-    raspberry-pi."4".apply-overlays-dtmerge.enable = true;
-    raspberry-pi."4".poe-plus-hat.enable = true;
-    deviceTree = {
-      enable = true;
-      filter = "*rpi-4-*.dtb";
-    };
-  };
+  hardware.raspberry-pi."4".poe-plus-hat.enable = true;
 }
