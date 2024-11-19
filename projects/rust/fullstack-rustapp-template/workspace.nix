tghanken/playground

commit 89e52ffb2b16a11984b6f5bf7af3c790cfedaa12

feat: decouple vite build and cargo build for frontend (#34)
Fixes PROV-55

projects/rust/fullstack-rustapp-template/workspace.nix
@@ -50,11 +50,7 @@
       };
 
       # Common arguments can be set here to avoid repeating them later
-      commonArgs =
-        baseArgs
-        // {
-          VITE_MANIFEST_PATH = "${vite-manifests}/manifest/manifest.json";
-        };
+      commonArgs = baseArgs;
 
       workspaceHackSource = lib.fileset.toSource {
         root = ./.;
@@ -156,24 +152,18 @@
       dockerImage = pkgs.dockerTools.streamLayeredImage {
         name = "fullstack-rustapp-server";
         tag = "latest";
-        contents = [
-          server
-          vite-runtime-files
-        ];
         config = {
           Cmd = ["${server}/bin/server"];
+          Env = ["VITE_MANIFEST_PATH=${vite-manifests}/manifest/manifest.json"];
         };
       };
 
       dockerDevImage = pkgs.dockerTools.streamLayeredImage {
         name = "fullstack-rustapp-server";
         tag = "latest";
-        contents = [
-          devServer
-          vite-runtime-files
-        ];
         config = {
           Cmd = ["${devServer}/bin/server"];
+          Env = ["VITE_MANIFEST_PATH=${vite-manifests}/manifest/manifest.json"];
         };
       };
     in {

projects/rust/fullstack-rustapp-template/packages/libs/frontend-ui/src/lib.rs
@@ -1,10 +1,14 @@
+use std::clone::Clone;
+use std::sync::LazyLock;
+
+use vite_manifest_parser::get_vite_references_str;
+
+use crate::utils::languages::SupportedLanguage;
+use crate::utils::make_asset_vector;
+
 pub mod pages;
 pub mod router;
 mod utils;
-
-use crate::utils::languages::SupportedLanguage;
-use once_cell::sync::Lazy;
-use vite_manifest_parser::{get_vite_references_str, ViteReferences};
 
 struct ApplicationMetadata {
     name: &'static str,
@@ -56,7 +60,17 @@ struct PageDetails<'a> {
     manifest_data: ManifestData,
 }
 
-static MANIFEST_STR: &str = include_str!(env!("VITE_MANIFEST_PATH"));
-
-static MANIFESTS: Lazy<ViteReferences> =
-    Lazy::new(|| get_vite_references_str(MANIFEST_STR).unwrap());
+static MANIFEST_DATA: LazyLock<ManifestData> = LazyLock::new(|| {
+    let manifest_path = std::env::var("VITE_MANIFEST_PATH")
+        .expect("VITE_MANIFEST_PATH environment variable must be set");
+    let manifest_str =
+        std::fs::read_to_string(&manifest_path).expect("Cannot read the vite manifest");
+    let parsed_manifest =
+        get_vite_references_str(manifest_str.as_str()).expect("Cannot parse the vite manifest");
+    let stylesheets = make_asset_vector(parsed_manifest.css_files.clone());
+    let scripts = make_asset_vector(parsed_manifest.js_files.clone());
+    ManifestData {
+        stylesheets,
+        scripts,
+    }
+});

projects/rust/fullstack-rustapp-template/packages/libs/frontend-ui/src/pages/dashboard.rs
@@ -1,8 +1,10 @@
-use crate::utils::languages::SupportedLanguage;
-use crate::utils::make_asset_vector;
-use crate::{ApplicationMetadata, ManifestData, PageDetails, PageMetadata, SentryData, MANIFESTS};
 use askama::Template;
 use askama_axum::IntoResponse;
+
+use crate::utils::languages::SupportedLanguage;
+use crate::{
+    ApplicationMetadata, ManifestData, PageDetails, PageMetadata, SentryData, MANIFEST_DATA,
+};
 
 #[derive(Template)]
 #[template(path = "pages/dashboard.html")]
@@ -24,8 +26,8 @@ pub async fn render_dashboard() -> impl IntoResponse {
                 lang: SupportedLanguage::English,
             },
             manifest_data: ManifestData {
-                stylesheets: make_asset_vector(MANIFESTS.css_files.clone()),
-                scripts: make_asset_vector(MANIFESTS.js_files.clone()),
+                stylesheets: Vec::from(&*MANIFEST_DATA.stylesheets),
+                scripts: Vec::from(&*MANIFEST_DATA.scripts),
             },
         },
     }

projects/rust/fullstack-rustapp-template/packages/libs/frontend-ui/src/pages/themes.rs
@@ -1,8 +1,10 @@
-use crate::utils::languages::SupportedLanguage;
-use crate::utils::make_asset_vector;
-use crate::{ApplicationMetadata, ManifestData, PageDetails, PageMetadata, SentryData, MANIFESTS};
 use askama::Template;
 use askama_axum::IntoResponse;
+
+use crate::utils::languages::SupportedLanguage;
+use crate::{
+    ApplicationMetadata, ManifestData, PageDetails, PageMetadata, SentryData, MANIFEST_DATA,
+};
 
 #[derive(Template)]
 #[template(path = "pages/themes.html")]
@@ -24,8 +26,8 @@ pub async fn render_themes() -> impl IntoResponse {
                 lang: SupportedLanguage::English,
             },
             manifest_data: ManifestData {
-                stylesheets: make_asset_vector(MANIFESTS.css_files.clone()),
-                scripts: make_asset_vector(MANIFESTS.js_files.clone()),
+                stylesheets: Vec::from(&*MANIFEST_DATA.stylesheets),
+                scripts: Vec::from(&*MANIFEST_DATA.scripts),
             },
         },
     }

projects/rust/fullstack-rustapp-template/packages/libs/frontend-ui/tailwind.config.ts
@@ -43,9 +43,7 @@ const cdark = {
 export default {
     mode: "all",
     content: [
-        "./src/**/*.{rs,html,css,js,ts}",
-        "./src_ts/**/*.{html,css,js,ts}",
-        "./dist/**/*.html",
+        "./src_ts/**/*.ts",
         "./templates/**/*.html"
     ],
     theme: {

projects/rust/fullstack-rustapp-template/packages/libs/frontend-ui/vite.nix
@@ -4,42 +4,56 @@
   dream2nix,
   nixpkgs,
   ...
-}: {
-  imports = [
-    dream2nix.modules.dream2nix.nodejs-package-lock-v3
-    dream2nix.modules.dream2nix.nodejs-granular-v3
+}: let
+  fs = lib.fileset;
+  sourceFiles = fs.unions [
+    ./input.css
+    ./package.json
+    ./package-lock.json
+    ./postcss.config.cjs
+    ./tailwind.config.ts
+    ./vite.config.ts
+    (fs.fileFilter (file: file.hasExt "ts") ./src_ts)
+    (fs.fileFilter (file: file.hasExt "html") ./templates)
   ];
+in
+  #  fs.trace sourceFiles
+  {
+    imports = [
+      dream2nix.modules.dream2nix.nodejs-package-lock-v3
+      dream2nix.modules.dream2nix.nodejs-granular-v3
+    ];
 
-  mkDerivation = {
-    src = ./.;
-    installPhase = ''
-      mkdir -p $manifest/manifest
-      cp ./dist/.vite/manifest.json $manifest/manifest/manifest.json
+    mkDerivation = {
+      src = fs.toSource {
+        root = ./.;
+        fileset = sourceFiles;
+      };
+      installPhase = ''
+        mkdir -p $manifest/manifest
+        cp ./dist/.vite/manifest.json $manifest/manifest/manifest.json
 
-      mkdir -p $assets/assets
-      cp -r ./dist/assets $assets
+        mkdir -p $assets/assets
+        cp -r ./dist/assets $assets
 
-      mkdir -p $sourcemaps/sourcemaps
-      mv $assets/assets/*.js.map $sourcemaps/sourcemaps
+        mkdir -p $sourcemaps/sourcemaps
+        mv $assets/assets/*.js.map $sourcemaps/sourcemaps
+      '';
+      outputs = ["out" "manifest" "assets" "sourcemaps"];
+    };
 
-      mkdir -p $node_modules/node_modules
-      cp -r ./node_modules $node_modules
-    '';
-    outputs = ["out" "manifest" "assets" "sourcemaps" "node_modules"];
-  };
+    deps = {nixpkgs, ...}: {
+      inherit
+        (nixpkgs)
+        fetchFromGitHub
+        stdenv
+        ;
+    };
 
-  deps = {nixpkgs, ...}: {
-    inherit
-      (nixpkgs)
-      fetchFromGitHub
-      stdenv
-      ;
-  };
+    nodejs-package-lock-v3 = {
+      packageLockFile = "${config.mkDerivation.src}/package-lock.json";
+    };
 
-  nodejs-package-lock-v3 = {
-    packageLockFile = "${config.mkDerivation.src}/package-lock.json";
-  };
-
-  name = "frontend-ui-vite-manifests";
-  version = "0.1.0";
-}
+    name = "frontend-ui-vite-manifests";
+    version = "0.1.0";
+  }

commit e457e2bbc5d61202879b93f0a452b7720985e5f3

fix: add vite-runtime-files to docker images (#38)
Fixes missing vite runtime files caused by #34

projects/rust/fullstack-rustapp-template/workspace.nix
@@ -152,6 +152,9 @@
       dockerImage = pkgs.dockerTools.streamLayeredImage {
         name = "fullstack-rustapp-server";
         tag = "latest";
+        contents = [
+          vite-runtime-files
+        ];
         config = {
           Cmd = ["${server}/bin/server"];
           Env = ["VITE_MANIFEST_PATH=${vite-manifests}/manifest/manifest.json"];
@@ -161,6 +164,9 @@
       dockerDevImage = pkgs.dockerTools.streamLayeredImage {
         name = "fullstack-rustapp-server";
         tag = "latest";
+        contents = [
+          vite-runtime-files
+        ];
         config = {
           Cmd = ["${devServer}/bin/server"];
           Env = ["VITE_MANIFEST_PATH=${vite-manifests}/manifest/manifest.json"];