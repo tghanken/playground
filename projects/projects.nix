0d095fb17f6b36bf676d4ec121a966ab5314169b
Taylor Hanken <tghanken@gmail.com>
2024-12-15 06:34:42 -0800

    chore: move default shell to projects folder (#60)

--- projects/projects.nix
+++ projects/projects.nix
@@ -2,4 +2,47 @@
   imports = [
     ./rust/fullstack-rustapp-template/workspace.nix
   ];
+
+  perSystem = {
+    pkgs,
+    system,
+    lib,
+    ...
+  }:
+    with inputs; let
+      inherit pkgs lib;
+      craneLib = crane.mkLib pkgs;
+    in {
+      devShells.default = craneLib.devShell {
+        # Inherit inputs from checks.
+        checks = self.checks.${system};
+
+        shellHooks = ''
+          git lfs install
+        '';
+
+        packages = with pkgs; [
+          # Core
+          alejandra
+          just
+          dive
+          nix-output-monitor
+          bacon
+          watchexec
+          git-lfs
+
+          # JS
+          pnpm
+          nodejs
+
+          # Rust
+
+          # CF Workers
+          cargo-generate
+          worker-build
+          wasm-pack
+          binaryen
+        ];
+      };
+    };
 }
