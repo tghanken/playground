68dec77424b9fa6fa99378f323c3c645d395cb18
Taylor Hanken <tghanken@gmail.com>
2024-12-19 07:19:12 -0800

    Run earthly fix command

--- projects/pocket-cellar/private-api/src/lib.rs
+++ projects/pocket-cellar/private-api/src/lib.rs
@@ -1,11 +1,7 @@
 use worker::*;
 
 #[event(fetch)]
-async fn fetch(
-    _req: HttpRequest,
-    _env: Env,
-    _ctx: Context,
-) -> Result<HttpResponse> {
+async fn fetch(_req: HttpRequest, _env: Env, _ctx: Context) -> Result<HttpResponse> {
     console_error_panic_hook::set_once();
     Ok(http::Response::builder()
         .status(http::StatusCode::OK)
