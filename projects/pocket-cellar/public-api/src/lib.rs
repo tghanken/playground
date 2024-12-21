616967083d32fd55d5d98c7fb4c8deb51952d16a
Taylor Hanken <tghanken@gmail.com>
2024-12-21 10:52:12 -0800

    WIP: call backend service from public api

--- projects/pocket-cellar/public-api/src/lib.rs
+++ projects/pocket-cellar/public-api/src/lib.rs
@@ -1,8 +1,9 @@
 use worker::*;
 
 #[event(fetch)]
-async fn fetch(_req: HttpRequest, _env: Env, _ctx: Context) -> Result<HttpResponse> {
+async fn fetch(_req: HttpRequest, env: Env, _ctx: Context) -> Result<HttpResponse> {
     console_error_panic_hook::set_once();
+    env.service("PRIVATE_API")?.fetch("/", None).await?;
     Ok(http::Response::builder()
         .status(http::StatusCode::OK)
         .body(Body::empty())?)
